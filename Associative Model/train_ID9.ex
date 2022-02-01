8f57367509f677ffd74407b33bde70e81897e036
JMiao2 <jmiao1@swarthmore.edu>
2022-02-01 15:45:18 -0500

    a

--- Associative Model/train_ID9.ex
+++ Associative Model/train_ID9.ex
@@ -5,18 +5,18 @@ I:
 (Color)
 0 0 1 0 0 0 0 0 0
 (Feature)
-0 1
-(Motion)
 1 0
+(Motion)
+0 1
 T:
 (Shape_OUT)
 0 0 1 0 0 0 0 0 0
 (Color_OUT)
 0 0 1 0 0 0 0 0 0
 (Feature_OUT)
-0 1
-(Motion_OUT)
 1 0
+(Motion_OUT)
+0 1
 ;
 
 name: SecondExample
@@ -26,18 +26,18 @@ I:
 (Color)
 1 0 0 0 0 0 0 0 0
 (Feature)
-0 1
-(Motion)
 1 0
+(Motion)
+0 1
 T:
 (Shape_OUT)
 1 0 0 0 0 0 0 0 0
 (Color_OUT)
 1 0 0 0 0 0 0 0 0
 (Feature_OUT)
-0 1
-(Motion_OUT)
 1 0
+(Motion_OUT)
+0 1
 ;
 
 name: ThirdExample
@@ -47,18 +47,18 @@ I:
 (Color)
 0 0 0 1 0 0 0 0 0
 (Feature)
-0 1
+1 0
 (Motion)
-0 1
+1 0
 T:
 (Shape_OUT)
 0 0 0 1 0 0 0 0 0
 (Color_OUT)
 0 0 0 1 0 0 0 0 0
 (Feature_OUT)
-0 1
+1 0
 (Motion_OUT)
-0 1 
+1 0
 ;
 
 name: FourthExample
@@ -68,18 +68,18 @@ I:
 (Color)
 0 1 0 0 0 0 0 0 0
 (Feature)
-0 1
+1 0
 (Motion)
-0 1
+1 0
 T:
 (Shape_OUT)
 0 1 0 0 0 0 0 0 0
 (Color_OUT)
 0 1 0 0 0 0 0 0 0
 (Feature_OUT)
-0 1
+1 0
 (Motion_OUT)
-0 1
+1 0
 ;
 
 name: FifthExample
@@ -91,7 +91,7 @@ I:
 (Feature)
 0 1
 (Motion)
-0 1
+1 0
 T:
 (Shape_OUT)
 0 0 0 0 0 1 0 0 0
@@ -100,7 +100,7 @@ T:
 (Feature_OUT)
 0 1
 (Motion_OUT)
-0 1
+1 0
 ;
 
 name: SixthExample
@@ -112,7 +112,7 @@ I:
 (Feature)
 0 1
 (Motion)
-0 1
+1 0
 T:
 (Shape_OUT)
 0 0 0 0 0 0 0 1 0
@@ -121,7 +121,7 @@ T:
 (Feature_OUT)
 0 1
 (Motion_OUT)
-0 1
+1 0
 ;
 
 name: SeventhExample
@@ -133,7 +133,7 @@ I:
 (Feature)
 1 0
 (Motion)
-1 0
+0 1
 T:
 (Shape_OUT)
 0 0 0 0 1 0 0 0 0
@@ -142,7 +142,7 @@ T:
 (Feature_OUT)
 1 0
 (Motion_OUT)
-1 0
+0 1
 ;
 
 name: EighthExample
@@ -154,15 +154,15 @@ I:
 (Feature)
 1 0
 (Motion)
-1 0
+0 1
 T:
 (Shape_OUT)
 0 0 0 0 0 0 1 0 0
 (Color_OUT)
 0 0 0 0 0 0 1 0 0
 (Feature_OUT)
-1 0 
-(Motion_OUT)
 1 0
+(Motion_OUT)
+0 1
 ;
 
